8b63f6ddba19fd247943cce7009b9586dead89d3
Larry <hej720@163.com>
2015-12-21 23:43:50 +0800

    Update

--- Tianma.py
+++ Tianma.py
@@ -4,6 +4,15 @@ Created on Tue Dec  8 22:49:49 2015
 
 @author: Administrator
 """
+#==============================================================================
+#==============================================================================
+# 说明:
+# 1.将Tianma.py和静态表拷贝到工作目录中；
+# 2.在工作目录下建立Data文件夹，将银行数据拷贝至该文件夹;该文件夹下所有的带有银行关键字
+# 的xls和xlsx文件都会被处理
+# 3.双击执行Tianma.py；会在工作目录下生成Result文件夹及分类结果
+#==============================================================================
+#==============================================================================
 
 from pandas import DataFrame, Series
 import pandas as pd
@@ -17,11 +26,17 @@ from os.path import join, getsize
 WorkPath = 'E:/_Projects/Personal/SVN/_Projects/Python/TianmaFinance'
 os.chdir(WorkPath)
 
-StatRule1 = pd.read_excel('静态表.xlsx',index_col = 0)
-StatRule2 = pd.read_excel('静态表.xlsx',1)
-ClassifyRuleDF = StatRule2.最终结果
-ClassifyRuleDF.index = [StatRule2.银行,StatRule2.收支,StatRule2.大类,StatRule2.关键字]
-ClassifyRuleDF.ix['招商银行'].ix['支出'].ix['对公售汇'].ix['无']
+WorkPath = os.getcwd()
+DataPath = WorkPath + '/Data'
+ResultPath = WorkPath + '/Result'
+if not os.path.exists('Result'):
+    os.mkdir('Result')  #如果无Result文件夹则创建Result文件夹
+StatRule1 = pd.read_excel(WorkPath + '/静态表.xlsx',index_col = 0)
+StatRule2 = pd.read_excel(WorkPath + '/静态表.xlsx',1)
+ClassifyRuleDF = StatRule2.ix[:,'最终结果']
+ClassifyRuleDF.index = [StatRule2.银行,StatRule2.收支,StatRule2.大类,StatRule2.关键字,StatRule2.对方户名]
+
+os.chdir(DataPath)
 
 class StatFileClass:
     def __init__(self,temp=0):
@@ -34,11 +49,13 @@ class StatFileClass:
             self.Currency = 'JPY'
         else:
             self.Currency = 'CNY'
+        self.DateLable = StatRule1.ix[self.BankName,'交易日期字段']
         self.IncomeLable = StatRule1.ix[self.BankName,'收入字段']
         self.PayLable = StatRule1.ix[self.BankName,'支出字段']
         self.BalanceLable = StatRule1.ix[self.BankName,'当日余额字段']
         self.KeyLable1 = StatRule1.ix[self.BankName,'大类字段']
-        self.KeyLable2 = StatRule1.ix[self.BankName,'子类字段']
+        self.KeyLable2 = StatRule1.ix[self.BankName,'子类字段'].split('+') #字符串list
+        self.CountLable = StatRule1.ix[self.BankName,'户名字段']
         self.SkipRows = StatRule1.ix[self.BankName,'数据开始行']-1
         
         if self.IncomeLable == self.PayLable: #中国银行,收入字段和支出字段相同
@@ -91,11 +108,11 @@ class StatFileClass:
         else:
             self.ResultDF = pd.concat([self.IncomeData,self.PayData,self.IncomeType,self.KeyWord1,self.KeyWord2,self.ClassifyResult],axis = 1)
             self.ResultDF.columns = ['收入','支出','收支类型','大类','子类','分类结果']
-        self.ResultFileName = self.FileNameHead + '分类结果.xlsx'       
+        self.ResultFileName = ResultPath + '/' +self.FileNameHead + '分类结果.xlsx'       
         self.ResultDF.to_excel(self.ResultFileName,self.FileNameHead)
 
 
-for FileName in os.listdir(WorkPath):
+for FileName in os.listdir(DataPath):
     if ('银行'  in FileName) and ('xls' in FileName) and ('分类结果' not in FileName):
         print('正在处理:' + FileName)
         StatFileClass(FileName);
